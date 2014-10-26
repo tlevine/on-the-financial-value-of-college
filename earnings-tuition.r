218160aad5f2744bd3e575eb9d29daee1454a1c5
Thomas Levine <_@thomaslevine.com>
2014-10-26 16:52:55 +0000

    -

--- earnings-tuition.r
+++ earnings-tuition.r
@@ -3,9 +3,15 @@ library(reshape2)
 library(scales)
 
 years.invested <- 65 - 20 # People supposedly retire at 65.
-stock.market.return <- 1.07
 prop.withdrawn <- 0.15 # Withdraw 15% of the initial investment every year, ignoring inflation
+stock.market.return <- c(1.06, 1.08)
 inflation <- 1.025 # 3% inflation
+
+earnings.slope <- function(stock.market.return, prop.withdrawn) {
+  f <- function(money, year) money *
+         (stock.market.return - (prop.withdrawn * inflation ^ year))
+  sum(Reduce(f, 1:years.invested, accumulate = TRUE, init = 1))
+}
 
 lifetime.earnings <- c(
   engineering = 2e6,
@@ -21,19 +27,20 @@ college <- data.frame(
   full.investment = paste0('College median\n(',
                            gsub('\\.', ' ', names(lifetime.earnings)), ')')
 )
+not.college <- data.frame(
+  investment = 'Not College',
+  label.x = 1.8e5,
+  earnings.intercept = 0,
+  earnings.slope = sapply(stock.market.return, function(rate)
+                          earnings.slope(rate, prop.withdrawn))
+)
+not.college$full.investment <- paste0(
+  'Stocks\n(', round(100 * (stock.market.return - 1)), '%)')
 
-earnings <- function(starting.money, stock.market.return, prop.withdrawn) {
-  f <- function(money, year) {
-    money * (stock.market.return - (prop.withdrawn * inflation ^ year))
-  }
-  g <- function(money) sum(Reduce(f, 1:years.invested, accumulate = TRUE, init = money))
-  sapply(starting.money, g)
-}
+both <- rbind(college, not.college)
 
-not.college <- data.frame(cost = seq(0, 2e5, 1e4))
-not.college$earnings <- earnings(not.college$cost, stock.market.return, prop.withdrawn)
-
-p.base <- ggplot() +
+p.base <- ggplot(both) +
+  aes(color = investment) +
   theme_minimal() +
   theme(title = element_text(face = 'bold'),
         panel.grid.minor = element_blank(),
@@ -43,26 +50,20 @@ p.base <- ggplot() +
   scale_y_continuous('Earnings (today dollars)',
                      limits = c(0, 2.5e6), labels = dollar)
 
-stock.market.label <- 'Stock market,
-assuming 7% annual increase
-with an inflation-adjusted (2.5%) salary
-starting at 15% of the cost of college'
+stock.label <- 'Stock market lines assume annual withdrawals
+that start at 15% the cost of college and increase
+by 2.5% per year.'
 p.predictions <- p.base +
-  geom_line(data = not.college,
-            color = 'grey20',
-            aes(x = cost, y = earnings)) +
-  geom_hline(data = college,
-             color = 'grey60',
-             aes(yintercept = earnings.intercept,
-                 group = full.investment)) +
-  annotate('text', x = 1.2e5, y = 5e5, label = stock.market.label, color = 'grey20') +
-  geom_text(data = college,
-            color = 'grey60',
-            aes(x = label.x,
-                y = earnings.intercept,
+  geom_abline(aes(slope = earnings.slope,
+                  intercept = earnings.intercept,
+                  group = full.investment)) +
+  geom_text(aes(x = label.x,
+                y = earnings.intercept + label.x * earnings.slope,
                 vjust = -.2,
                 lineheight = .8,
                 label = full.investment)) +
+  annotate('text', x = 2e5, y = 1e5, label = stock.label,
+           hjust = 1, vjust = 0) +
   ggtitle('Predicted return on college and stock market investments')
 
 p.tom.expenses <- p.base + geom_vline(xintercept = 1e5) +
@@ -89,8 +90,8 @@ ppplot <- function(plot, filename) ggsave(filename = filename,
                                           plot = plot,
                                           width = 8, height = 6,
                                           units = 'in', dpi = 200)
-ppplot(p.predictions, 'general-predictions.png')
-ppplot(p.tom.expenses, 'tom-expenses.png')
-ppplot(p.tom.predictions, 'tom-predictions.png')
-ppplot(p.tom.both, 'tom-both.png')
-ppplot(p.tom.comparison, 'tom-comparison.png')
+#ppplot(p.predictions, 'general-predictions.png')
+#ppplot(p.tom.expenses, 'tom-expenses.png')
+#ppplot(p.tom.predictions, 'tom-predictions.png')
+#ppplot(p.tom.both, 'tom-both.png')
+#ppplot(p.tom.comparison, 'tom-comparison.png')
