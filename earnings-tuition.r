dc2c1c8c06fe37fa5205cc2f2a1ffd27feb76966
Thomas Levine <_@thomaslevine.com>
2014-10-27 11:23:51 +0000

    update

--- earnings-tuition.r
+++ earnings-tuition.r
@@ -6,11 +6,23 @@ years.invested <- 65 - 20 # People supposedly retire at 65.
 prop.withdrawn <- 0.15 # Withdraw 15% of the initial investment every year, ignoring inflation
 stock.market.return <- c(1.06, 1.08)
 inflation <- 1.025 # 3% inflation
+median.no.college <- 5.8e5
+
+colors <- c(tom = '#fe57a1',
+            college = 'grey60',
+            work = '#57FD8F',
+            retire = '#F1FF57')
 
 earnings.slope <- function(stock.market.return, prop.withdrawn) {
-  f <- function(money, year) money *
-         (stock.market.return - (prop.withdrawn * inflation ^ year))
-  sum(Reduce(f, 1:years.invested, accumulate = TRUE, init = 1))
+  f <- function(state, year) {
+    withdrawal <- prop.withdrawn * inflation ^ year
+    new.state <- list(money = state$money * (stock.market.return - withdrawal))
+    new.state$withdrawals = state$withdrawals + withdrawal
+    new.state
+  }
+  begin.state <- list(money = 1, withdrawals = 0)
+  end.state <- Reduce(f, 1:years.invested, init = begin.state)
+  end.state$money + end.state$withdrawals
 }
 
 lifetime.earnings <- c(
@@ -20,28 +32,41 @@ lifetime.earnings <- c(
 )
 
 college <- data.frame(
-  investment = 'College',
+  investment = 'College and work',
   label.x = 1.8e5,
   earnings.intercept = unname(lifetime.earnings),
   earnings.slope = 0,
+  label.y = unname(lifetime.earnings),
   full.investment = paste0('College median\n(',
                            gsub('\\.', ' ', names(lifetime.earnings)), ')')
 )
-not.college <- data.frame(
+
+retire <- data.frame(
   investment = 'Stock market',
-  label.x = 1.8e5,
+  label.x = 1.5e5,
   earnings.intercept = 0,
-  earnings.slope = sapply(stock.market.return, function(rate)
+  earnings.slope = sapply(min(stock.market.return), function(rate)
                           earnings.slope(rate, prop.withdrawn))
 )
-not.college$full.investment <- paste0(
-  'Stocks\n(', round(100 * (stock.market.return - 1)), '%)')
+retire$label.y <- 0.9 * retire$label.x * retire$earnings.slope
+retire$full.investment <- paste0(
+  'Stocks\nwith\nwithdrawals\n(', round(100 * (min(stock.market.return) - 1)), '%)')
 
-both <- rbind(college, not.college)
+work <- data.frame(
+  investment = 'Stock market and work',
+  label.x = 3e4,
+  earnings.intercept = 0,
+  earnings.slope = sapply(stock.market.return, function(rate)
+                          earnings.slope(rate, 0))
+)
+work$label.y <- 2 * work$label.x * work$earnings.slope
+work$full.investment <- paste0(
+  'Stocks\nwithout\nwithdrawals\n(', round(100 * (stock.market.return - 1)), '%)')
+
+both <- rbind(college, retire, work)
 
 p.base <- ggplot(both) +
-  aes(color = investment) +
-  scale_color_manual(values = c('grey60', 'grey20'),
+  scale_color_manual(values = unname(c(colors['college'], colors['retire'], colors['work'])),
                      name = 'Investment type') +
   theme_minimal() +
   theme(title = element_text(face = 'bold'),
@@ -52,20 +77,32 @@ p.base <- ggplot(both) +
   scale_y_continuous('Earnings (today dollars)',
                      limits = c(0, 2.5e6), labels = dollar)
 
-stock.label <- 'Stock investment lines assume annual withdrawals that
+stock.retire.label <- 'Stock investment lines assume annual withdrawals that
 start at 15% the cost of college and increase by 2.5% per year.
 (This serves as a salary.)'
+
+no.college.salary <- round(median.no.college / years.invested / 1000)
+stock.work.label <- paste0(
+'Stock investments without withdrawals assume that you work
+a job that pays your living expenses but does not let you
+save money. On median, you can expect a yearly salary starting
+at $', no.college.salary, ',000 and increasing with inflation.')
+
 p.predictions <- p.base +
   geom_abline(aes(slope = earnings.slope,
+                  color = investment,
                   intercept = earnings.intercept,
                   group = full.investment)) +
   geom_text(aes(x = label.x,
-                y = earnings.intercept + label.x * earnings.slope,
-                vjust = -.2,
+                y = label.y,
+                color = investment,
+                vjust = 1.2,
                 lineheight = .8,
                 label = full.investment)) +
-  annotate('text', x = 2e5, y = 1e5, label = stock.label,
+  annotate('text', x = 2e5, y = 1e5, label = stock.retire.label,
            hjust = 1, vjust = 0) +
+  annotate('text', x = 0, y = 2.5e6, label = stock.work.label,
+           hjust = 0, vjust = 1) +
   ggtitle('Predicted return on college and stock market investments')
 
 p.tom.expenses <- p.base + geom_vline(xintercept = 1e5) +
@@ -78,13 +115,13 @@ p.tom.both <- p.base +
                 geom_vline(xintercept = 1e5) +
                 geom_hline(yintercept = 1.15e6) +
                 aes(x = 1e5, y = 1.19e6) +
-                geom_point(color = '#fe57a1', size = 20) +
+                geom_point(color = colors['tom'], size = 20) +
                 geom_text(color = 'white', label = 'Tom') +
                 ggtitle("Tom's college expenses and predicted earnings.")
 
 p.tom.comparison <- p.predictions +
   aes(x = 1e5, y = 1.19e6) +
-  geom_point(color = '#fe57a1', size = 20) +
+  geom_point(color = colors['tom'], size = 20) +
   geom_text(color = 'white', label = 'Tom') +
   ggtitle("How Tom's investment compares to alternatives")
 
